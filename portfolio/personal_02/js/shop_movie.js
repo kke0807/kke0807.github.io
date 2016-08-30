8e1240a6e39c98fc1c9fe1560c5525e257ab78ea
kke0807 <kke0807@gmail.com>
2016-08-30 17:03:02 +0900

    Add files via upload

---
+++ portfolio/personal_02/js/shop_movie.js
@@ -0,0 +1,9 @@
+//shop_movie.js
+ function shop_movie(){
+     var shopmovieWrap=$('#shopmovieWrap'),
+         play_btn = $('.play_btn');
+    play_btn.on('click',function(){
+    var $this = $(this);
+     $this.parent().html('<iframe width="100%" height="350px" src="https://www.youtube.com/embed/M58dcf6jbtc?autoplay=1&loop=1" frameborder="0" allowfullscreen>');  
+    }); 
+ };
